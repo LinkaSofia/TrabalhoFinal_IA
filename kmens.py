eec5dfa909cc9a448b5d81ea51b77c4418c64cf9
LinkaSofia <linkasofialunkes@gmail.com>
2023-12-18 21:06:19 -0300

    Imprimndo informações da imagem

--- kmens.py
+++ kmens.py
@@ -29,10 +29,17 @@ for imagem in lista_imagens:
     # Aplicando K-means
     kmeans = KMeans(n_clusters=clusters, n_init=10, random_state=42)
     kmeans.fit(unidimensional)
-    
+
     # Obtém os rótulos de cluster e os centros dos clusters para reconstruir a imagem segmentada
     segmented_image = kmeans.cluster_centers_[kmeans.labels_]
     segmented_image = segmented_image.reshape(image.shape)
     
     # Escreve a imagem segmentada em um novo arquivo
     cv2.imwrite(f'Imagem_Segmentada_{clusters}_{imagem}', segmented_image)
+    
+    print('------------------------------------------------------------')
+    print(f'Cores na imagem antes do processamento:', len(np.unique(image)))
+    print(f'Cores na imagem:', len(np.unique(segmented_image)))  # retorna quantidade de cores diferentes
+
+    file_size = os.path.getsize(f'Imagem_Segmentada_{clusters}_{imagem}')
+    print("O tamanho do arquivo :", file_size/1000, "Kbytes") 
