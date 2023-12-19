8c139cd05a39057b42bfb8c78e111ca385a36554
LinkaSofia <linkasofialunkes@gmail.com>
2023-12-18 21:05:13 -0300

    Obtendo rótulos e imprimindo segmentação

--- kmens.py
+++ kmens.py
@@ -29,3 +29,10 @@ for imagem in lista_imagens:
     # Aplicando K-means
     kmeans = KMeans(n_clusters=clusters, n_init=10, random_state=42)
     kmeans.fit(unidimensional)
+    
+    # Obtém os rótulos de cluster e os centros dos clusters para reconstruir a imagem segmentada
+    segmented_image = kmeans.cluster_centers_[kmeans.labels_]
+    segmented_image = segmented_image.reshape(image.shape)
+    
+    # Escreve a imagem segmentada em um novo arquivo
+    cv2.imwrite(f'Imagem_Segmentada_{clusters}_{imagem}', segmented_image)
